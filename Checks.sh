ca50b25e632b24afd87de4e64db74250074d4801
Joseph Hughes <joseph.hughes@glasgow.ac.uk>
2016-09-05 14:20:42 +0100

    adding diamond command

--- Checks.sh
+++ Checks.sh
@@ -3,3 +3,20 @@
 # 2) 30% similarity for protein (DIAMOND)
 # 3) 50% similarity for nucleotide
 # 4) low complexity (DUST, entropy)
+
+usage=`echo -e "\n Usage: Checks.sh file_R1.fastq file_R2.fastq ContigFile.fa Outprefix\n"`;
+
+if [[ ! $1 ]]
+then
+        printf "${usage}\n\n";
+exit;
+fi
+
+read1=$1
+read2=$2
+echo "Checking files" $read1 $read2
+contig=$3
+stub=$4
+
+Step 2 DIAMOND blastx ...
+diamond blastx -d $DIAMOND_DB/nr -p 8 -q $contig -a ${contig}_diamond -t ${stub}_temp_dir --top 1
