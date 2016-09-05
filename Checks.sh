3ce621e036c4b8c35863dc49d0f98b8fa6a05369
Joseph Hughes <joseph.hughes@glasgow.ac.uk>
2016-09-05 11:28:39 +0100

    checks

---
+++ Checks.sh
@@ -0,0 +1,5 @@
+# Pipeline to make the checks and gather stats to put into the database
+# 1) adaptor library
+# 2) 30% similarity for protein (DIAMOND)
+# 3) 50% similarity for nucleotide
+# 4) low complexity (DUST, entropy)
