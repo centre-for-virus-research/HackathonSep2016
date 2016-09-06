7186dc5bac68d909e61bc3e2f686790c1028811f
sejmodha <sbm12uol@gmail.com>
2016-09-06 13:48:55 +0100

    checks updated to use latest version of diamond

--- Checks.sh
+++ Checks.sh
@@ -18,31 +18,41 @@ then
 exit;
 fi
 
+if [[ ! -f $1 ]]
+then
+		pwd=`pwd`
+		echo Input file $1 not found, please ensure that the input file exists in $pwd;
+		exit;
+fi
+
 contig=$1
 stub=$2
 adapters=/home3/Davison/adapters.fasta
 diamonddb=/home3/Davison/db/
+diamond=/home3/Davison/bin/
+
 
 #Step 1 Adaptor library
-#to format the adapter file to blast db format
+#format the adapter file to blast db format
 
 echo "Mapping the sequences to adapter library"
 makeblastdb -in $adapters -dbtype 'nucl'
 
-blastn -db adapters.fasta -query $contig -out ${contig}_adapters_out -perc_identity 100 -outfmt 6 -num_threads 8
-cat ${contig}_adapters_out |cut -f1,2 > ${contig}_adapters_out_trimmed 
+blastn -db adapters.fasta -query $contig -out ${stub}_adapters_out -perc_identity 100 -outfmt 6 -num_threads 8
+#cat ${contig}_adapters_out |cut -f1,2 > ${contig}_adapters_out_trimmed 
 
 #Step 2 DIAMOND blastx and blastn classification of contigs
 
 echo "Classifying the sequences using DIAMOND and BLASTN.... this might take a while"
 mkdir ${stub}_temp_dir
-diamond blastx -d $diamonddb/nr -p 8 -q $contig -a ${contig}_diamond -t ${stub}_temp_dir --top 1 &
+${diamond}diamond blastx -d ${diamonddb}nr -q $contig -o ${stub}_diamond.m8 -t ${stub}_temp_dir --top 1 -p 8 &
 
-blastn -query $contig -db nt -out ${contig}_blastn.txt -evalue 0.0001 -max_target_seqs 1 -outfmt 6 -num_threads 8
-
-diamond view -a ${contig}_diamond.daa -o ${contig}_diamond.m8
+blastn -query $contig -db nt -out ${stub}_blastn.m8  -max_target_seqs 1 -outfmt 6 -num_threads 8
 
 #Step 3 filter blast and diamond output 
-echo "Filtering the classification output"
-awk -F"\t" '{if($3<=50) print}' ${contig}_blastn.txt > ${contig}_blastn_filtered.txt
-awk -F"\t" '{if($3<=30) print}' ${contig}_diamond.m8 > ${contig}_diamond_filtered.txt
+#echo "Filtering the classification output"
+#awk -F"\t" '{if($3<=50) print}' ${contig}_blastn.txt > ${contig}_blastn_filtered.txt
+#awk -F"\t" '{if($3<=30) print}' ${contig}_diamond.m8 > ${contig}_diamond_filtered.txt
+
+#wait for the diamond and the blastn is finished
+wait
