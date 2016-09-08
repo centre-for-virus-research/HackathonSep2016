4a449679f5d8824019e0b2c99d9100e9a9030ed5
U-CAMPUS\mm243e <mm243e@LN-4439C4554C90.campus.gla.ac.uk>
2016-09-08 16:30:25 +0100

    Updating contigs.php to print out results to browser

--- contigs.php
+++ contigs.php
@@ -4,13 +4,8 @@ $username = "hack";
 $password = "HackCVR16";
 $dbname = "Hack";
 
-//$id=$argv[1];
-$id ="midge1-0167e2:idba.genome1_contig-121_1027";
-echo "ContigID: " . $id . "\n";
-
-$outputFile = " ". $id . ".fasta";
-
-echo "FileName: " . $outputFile;
+$id=$argv[1];
+//$id ="midge1-0167e2:idba.genome1_contig-121_1027";
 
 
 // Create connection
@@ -27,23 +22,13 @@ $result = mysqli_query($conn, $sql);
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        echo ">" . $row["ContigID"]. "\n" . $row["Seq"].  "\n";
-       // Open the file to get existing content
-      // $current = file_get_contents($outputFile);
-       // Append a new person to the file
-       //$current .= ">" . $row["ContigID"]. "\n" . $row["Seq"].  "\n";
-       $current = ">" . $row["ContigID"]. "\n" . $row["Seq"].  "\n";
-       // Write the contents back to the file
-       file_put_contents($outputFile, $current);
-       
+        echo ">" . $row["ContigID"]. "\n" . $row["Seq"].  "\n";            
     }
 } else {
     echo "0 results\n";
 }
 
 mysqli_close($conn);
-
- 
 
 
 ?>
