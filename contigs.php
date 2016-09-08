e7a001c8035a979af5b03031c5b5f39824d60b2d
U-CAMPUS\mm243e <mm243e@LN-4439C4554C90.campus.gla.ac.uk>
2016-09-08 14:37:27 +0100

    updated contigs.php

--- contigs.php
+++ contigs.php
@@ -4,9 +4,17 @@ $username = "hack";
 $password = "HackCVR16";
 $dbname = "Hack";
 
-$id=$argv[1];
+//$id=$argv[1];
+$id ="midge1-0167e2:idba.genome1_contig-121_1027";
+echo "ContigID: " . $id . "\n";
+
+$outputFile = " ". $id . ".fasta";
+
+echo "FileName: " . $outputFile;
+
 
 // Create connection
+
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
 if (!$conn) {
@@ -20,11 +28,22 @@ if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
         echo ">" . $row["ContigID"]. "\n" . $row["Seq"].  "\n";
+       // Open the file to get existing content
+      // $current = file_get_contents($outputFile);
+       // Append a new person to the file
+       //$current .= ">" . $row["ContigID"]. "\n" . $row["Seq"].  "\n";
+       $current = ">" . $row["ContigID"]. "\n" . $row["Seq"].  "\n";
+       // Write the contents back to the file
+       file_put_contents($outputFile, $current);
+       
     }
 } else {
     echo "0 results\n";
 }
 
 mysqli_close($conn);
+
+ 
+
 
 ?>
